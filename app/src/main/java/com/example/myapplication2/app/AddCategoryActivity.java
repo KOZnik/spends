39c8ebffefca0aaf8a1b6ab2a62d6a9031a5938d
KOZnik <stanczykluk@gmail.com>
2015-02-09 23:07:51 +0100

    Initial comit

---
+++ app/src/main/java/com/example/myapplication2/app/AddCategoryActivity.java
@@ -0,0 +1,26 @@
+package com.example.myapplication2.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+public class AddCategoryActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_category);
+    }
+
+    public void addPosition(View view) {
+        LinearLayout positionsLayout = (LinearLayout) findViewById(R.id.positions);
+        EditText position = new EditText(this);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        position.setLayoutParams(params);
+        position.setHint(R.string.position_name);
+        positionsLayout.addView(position);
+    }
+}
